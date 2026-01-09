5c83cfdd716356fbba6e5152bc14071b22eeda03
Eternal-Snowstorm <3512546747@qq.com>
2026-01-09 21:28:22 +0100

    我有点懵, 为什么没法注入这个配方

---
+++ kubejs/server_scripts/recipes/tconstruct/Modifier.js
@@ -0,0 +1,62 @@
+ServerEvents.highPriorityData((event) => {
+
+    /** 
+        * 
+        * @param {String} modifier
+        * @param {Set} inputs
+        * @param {String} targetTag 
+        * @param {String} path
+        * @returns
+       */
+
+    function addCustomModifierRecipe(modifier, inputs, targetTag, path) {
+        let recipe = {
+            "type": "tconstruct:modifier",
+            "allow_crystal": true,
+            "check_trait_level": true,
+            "inputs": inputs,
+            "level": 1,
+            "result": modifier,
+            "slots": {
+                "abilities": 1
+            },
+            "tools": {
+                "tag": targetTag
+            }
+        }
+        return event.addJson(path, recipe)
+    }
+
+    addCustomModifierRecipe("nebula_tinker:acupoint", [
+        { "item": "cmi:blackstone_source_alpha" },
+        { "item": "cmi:blackstone_source_beta" },
+        { "item": "cmi:blackstone_source_gamma" },
+        { "tag": "forge:gems/charged_amethyst" },
+        { "tag": "forge:gems/charged_amethyst" }
+    ],
+        "tconstruct:modifiable/durability",
+        "tconstruct:modifier/ability/acupoint.json"
+    )
+
+    addCustomModifierRecipe("nebula_tinker:frenzy", [
+        { "tag": "forge:dusts/quartz" },
+        { "tag": "create:mechanisms/cobalt" },
+        { "tag": "forge:dusts/quartz" },
+        { "tag": "forge:gems/charged_amethyst" },
+        { "tag": "forge:gems/charged_amethyst" }
+    ],
+        "tconstruct:modifiable/durability",
+        "tconstruct:modifier/ability/frenzy.json"
+    )
+
+    addCustomModifierRecipe("nebula_tinker:causal_truncation", [
+        { "tag": "forge:plates/uranium" },
+        { "tag": "create:mechanisms/nether" },
+        { "tag": "forge:plates/uranium" },
+        { "tag": "forge:slimeball/blood" },
+        { "tag": "forge:slimeball/blood" }
+    ],
+        "tconstruct:modifiable/melee/primary",
+        "tconstruct:modifier/ability/causal_truncation.json"
+    )
+})
