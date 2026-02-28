dc8c853ca55bf62000c65cd707171896c05904c7
Qi-Month <dy757410@163.com>
2026-02-28 16:36:01 +0800

    jsdoc

--- kubejs/server_scripts/event/entity/attribute/Health.js
+++ kubejs/server_scripts/event/entity/attribute/Health.js
@@ -2,11 +2,12 @@ EntityEvents.spawned((event) => {
 	let { entity } = event
 
 	/**
-	 * @type {{ [key: Special.EntityType]: number }}
+	 * @type {Partial<Record<Special.EntityType, number>>}
 	 */
 	let customHealth = {
 		"minecraft:ender_dragon": 2000
 	}
+
 	let value = customHealth[entity.getType()]
 
 	if (!entity.isLiving()) {
