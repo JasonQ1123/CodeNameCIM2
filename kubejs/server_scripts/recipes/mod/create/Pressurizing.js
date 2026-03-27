3b78dc49e8454e541a16a0c6cf3f263f14a9d378
Qi-Month <dy757410@163.com>
2026-03-27 16:58:06 +0800

    莫名其妙的写法

--- kubejs/server_scripts/recipes/mod/create/Pressurizing.js
+++ kubejs/server_scripts/recipes/mod/create/Pressurizing.js
@@ -2,20 +2,17 @@ ServerEvents.recipes((event) => {
 	let { create, vintageimprovements } = event.getRecipes()
 
 	// 不稳红石
-	vintageimprovements.pressurizing(
-		Fluid.of("thermal:redstone", 125), [
+	vintageimprovements.pressurizing(Fluid.of("thermal:redstone", 125), [
 		"#forge:dusts/redstone"
 	]).heatRequirement(CmiHeatLevel.HEATED)
 
 	// 硫酸
-	vintageimprovements.pressurizing(
-		Fluid.of("mekanism:sulfuric_acid", 1000), [
+	vintageimprovements.pressurizing(Fluid.of("mekanism:sulfuric_acid", 1000), [
 		Fluid.of("minecraft:water", 1000),
 		Fluid.of("mekanism:sulfur_trioxide", 1000)
 	]).processingTime(500).id("vintageimprovements:pressurizing/sulfuric_acid")
 
-	vintageimprovements.pressurizing(
-		Fluid.of("tconstruct:molten_iron", 90), [
+	vintageimprovements.pressurizing(Fluid.of("tconstruct:molten_iron", 90), [
 		[
 			"#forge:dusts/iron",
 			"#create:crushed_raw_materials/iron"
