ef67e82bb5858add477c57742ead9e6af03acbbf
Qi-Month <dy757410@163.com>
2026-04-11 21:59:58 +0800

    依旧格式化

--- kubejs/server_scripts/recipes/mod/farmersdelight/Remove.js
+++ kubejs/server_scripts/recipes/mod/farmersdelight/Remove.js
@@ -1,10 +1,10 @@
 ServerEvents.recipes((event) => {
-    let removeRecipeIds = [
-        "farmersdelight:cutting/sugar_cane_alt"
-    ]
-    removeRecipeIds.forEach((id) => {
-        event.remove({
-            id: id
-        })
-    })
+	let removeRecipeIds = [
+		"farmersdelight:cutting/sugar_cane_alt"
+	]
+	removeRecipeIds.forEach((id) => {
+		event.remove({
+			id: id
+		})
+	})
 })
