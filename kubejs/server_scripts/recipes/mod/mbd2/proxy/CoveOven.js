9017b551ab4c087e5efce00e0dda9731092804d7
Qi-Month <dy757410@163.com>
2026-07-05 19:29:55 +0800

    MULTIBLOCKED!!!!!!!!!!!!!

--- kubejs/server_scripts/recipes/mod/mbd2/proxy/CoveOven.js
+++ kubejs/server_scripts/recipes/mod/mbd2/proxy/CoveOven.js
@@ -1,37 +1,37 @@
-let $CokeOvenRecipe =
-	Java.loadClass("blusunrize.immersiveengineering.api.crafting.CokeOvenRecipe")
+// let $CokeOvenRecipe =
+// 	Java.loadClass("blusunrize.immersiveengineering.api.crafting.CokeOvenRecipe")
 
-MBDRecipeTypeEvents.onTransferProxyRecipe("cmi:reinforced_coke_oven", (event) => {
-	let recipeEvent = event.getEvent()
-	let { proxyRecipe, proxyRecipeId, proxyTypeId, recipeType } = recipeEvent
+// MBDRecipeTypeEvents.onTransferProxyRecipe("cmi:reinforced_coke_oven", (event) => {
+// 	let recipeEvent = event.getEvent()
+// 	let { proxyRecipe, proxyRecipeId, proxyTypeId, recipeType } = recipeEvent
 
-	if (proxyTypeId !== "immersiveengineering:coke_oven") {
-		return
-	}
+// 	if (proxyTypeId !== "immersiveengineering:coke_oven") {
+// 		return
+// 	}
 
-	/**
-	 * @type {Internal.CokeOvenRecipe}
-	 */
-	let recipe = proxyRecipe
+// 	/**
+// 	 * @type {Internal.CokeOvenRecipe}
+// 	 */
+// 	let recipe = proxyRecipe
 
-	if (!(recipe instanceof $CokeOvenRecipe)) {
-		return
-	}
+// 	if (!(recipe instanceof $CokeOvenRecipe)) {
+// 		return
+// 	}
 
-	/**
-	 * @type {Internal.MBDRecipeSchema$MBDRecipeJS}
-	 */
-	let builder = recipeType.recipeBuilder()
+// 	/**
+// 	 * @type {Internal.MBDRecipeSchema$MBDRecipeJS}
+// 	 */
+// 	let builder = recipeType.recipeBuilder()
 
-	let creosote = "immersiveengineering:creosote"
-	let input = recipe.input.getBaseIngredient()
-	let output = recipe.output.get()
+// 	let creosote = "immersiveengineering:creosote"
+// 	let input = recipe.input.getBaseIngredient()
+// 	let output = recipe.output.get()
 
-	builder.inputItems(input)
-		.outputItems(output)
-		.outputFluids(Fluid.of(creosote, recipe.creosoteOutput))
-		.duration(recipe.time)
-		.id(proxyRecipeId + "_mbd2un")
+// 	builder.inputItems(input)
+// 		.outputItems(output)
+// 		.outputFluids(Fluid.of(creosote, recipe.creosoteOutput))
+// 		.duration(recipe.time)
+// 		.id(proxyRecipeId + "_mbd2un")
 
-	recipeEvent.mbdRecipe = builder.buildMBDRecipe()
-})
+// 	recipeEvent.mbdRecipe = builder.buildMBDRecipe()
+// })
